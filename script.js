6f673f80fd53c357a1306a7dd6e7a853bc2730bb
Shohei <shohei51@gmail.com>
2017-12-08 08:40:14 +0000

    basic chart finished

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// tutorial url https://bost.ocks.org/mike/bar/
+// tutorial url https://bost.ocks.org/mike/bar/2/
 const url='https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+const chartHeight=0.5; //as percentage of viewport height
+const chartWidth=0.8; //as percentage of viewport width
+
 
 $.get(url, function(result) {
     handleData(result.data);
@@ -8,6 +11,27 @@ $.get(url, function(result) {
 
 function handleData(data) {
     const title = 'US GDP Over Time';
-
-    console.log(title,data);
+    let dates = [];
+    let vals = [];
+    
+    d3.select(".title")
+        .append("h2")
+        .html(title);
+    
+    data.forEach(function(item){
+        dates.push(item[0]);
+        vals.push(Number(item[1]));
+    });
+    
+    const colWidth=($(window).width()*chartWidth/data.length)+'px';
+    const k=($(window).height()*chartHeight)/(Math.max(...vals));
+    
+    d3.select(".chart")
+    .selectAll("div")
+     .data(vals)
+    .enter().append("div")
+     .style("display", "inline-block")
+     .style("height", function(d) { return d*k + "px"; })
+     .style("width", function() {return colWidth;})
+     .style("background-color", "blue");
 }
