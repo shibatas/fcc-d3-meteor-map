66678a739e810ed66ba6af491373ec671093eeb9
Shohei <shohei51@gmail.com>
2017-12-20 08:34:24 +0000

    finished

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const svgWidth = $(window).width() > 1000 ? 700 : $(window).width()*0.8;  // wid
 const svgHeight = svgWidth*2/3;  //height of plot area
 const legendLeft = ($(window).width() - svgWidth)/2 + svgWidth-200;
 
+const month = (numMonth) => {
+    const months = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC'];
+    return months[numMonth-1];
+};
+
+const normalize = (val, max, min) => {
+    return (val-min)/(max-min);
+};
+
 console.log($(window).width() > 1000 ? 700 : $(window).width()*0.8 );
 
 $.get(url, function(result) {
@@ -13,18 +22,19 @@ $.get(url, function(result) {
 }, 'json')
     .fail(function(){ alert('error') });
 
-function handleData(data) {
+const handleData = (data) => {
   console.log(data);
     const title = "Monthly Global Land-Surface Temperature";
     const subtitle = "1753 - 2015";
     let AxisValX = [];
-    let AxisValY = [];
+    let AxisValY = [0];
     let val = [];
     data.monthlyVariance.forEach(function(item) {
         AxisValX.push(item.year);
         AxisValY.push(item.month);
         val.push(item.variance);
     });    
+    const max=d3.max(val), min=d3.min(val);
 
     console.log(AxisValX, AxisValY, val);
 
@@ -37,11 +47,10 @@ function handleData(data) {
     const margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = svgWidth - margin.left - margin.right,
     height = svgHeight - margin.top - margin.bottom;
-
-    // set the ranges
-    const x = d3.scaleTime().range([0, width]);
-    const y = d3.scaleLinear().range([height, 0]);
-              
+    
+    const gridWidth = width/(d3.max(AxisValX)-d3.min(AxisValX)),
+    gridHeight = height/12;
+    console.log(gridWidth,gridHeight);
     // append the svg object to the body of the page
     // append a 'group' element to 'svg'
     // moves the 'group' element to the top left margin
@@ -53,15 +62,21 @@ function handleData(data) {
         .attr("transform", 
               "translate(" + margin.left + "," + margin.top + ")");
 
+    const x = d3.scaleLinear().range([0, width]);
+    const y = d3.scaleLinear().range([height, 0]);
     x.domain(d3.extent(AxisValX));
     y.domain(d3.extent(AxisValY));
     
     // heat map
-    const colors = ["#a50026","#d73027","#f46d43","#fdae61","#fee090","#e0f3f8","#abd9e9","#74add1","#4575b4","#313695"];
-    const colorScale = d3.scale.quantile()
-    .domain(d3.extent(val))
-    .range(colors);
-
+    let cells = svg.selectAll('rect')
+        .data(data.monthlyVariance)
+        .enter().append('g').append('rect')
+        .attr('class', 'cell')
+        .attr('width', gridWidth)
+        .attr('height', gridHeight)
+        .attr('y', function(d) { return y(d.month); })
+        .attr('x', function(d) { return x(d.year); })
+        .attr('fill', 'yellow');
 
     // add the x Axis
     const xAxis = d3.axisBottom(x)
@@ -81,21 +96,21 @@ function handleData(data) {
     svg.append("g")
       .call(yAxis);
       
+    const colorScale = d3.scaleSequential(d3.interpolateSpectral).domain([1, 0]);
+
+    cells.transition().duration(1000)
+              .style("fill", function(d) { return colorScale(normalize(d.variance,max,min)); });
+      
     //tooltip
     let toolTip = d3.select(".contents").append("div").attr("class", "toolTip");
     
-    dots.on("mousemove", function(d){
+    cells.on("mousemove", function(d){
             toolTip.style("left", d3.event.pageX+10+"px");
             toolTip.style("top", d3.event.pageY-25+"px");
             toolTip.style("display", "inline-block");
-            toolTip.html((d.Name)+"<br>"+d.Time);
+            toolTip.html("variance: "+d.variance.toFixed(3)+"<br/>"+month(d.month)+"-"+d.year);
         });
-    dots.on("mouseout", function(d){
+    cells.on("mouseout", function(d){
             toolTip.style("display", "none");
         });
-}
-
-const month = (numMonth) => {
-    const months = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC'];
-    return months[numMonth-1];
 }
