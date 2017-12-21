57d35edc65ca3fc69849e23eb4b7a15a12679314
shibatas <shohei51@gmail.com>
2017-12-21 21:39:24 +0100

    map zoom and drag

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// https://bl.ocks.org/mbostock/4062045
+// http://bl.ocks.org/almccon/fe445f1d6b177fd0946800a48aa59c71
 
 $(document).ready(function() {
 
@@ -6,20 +6,47 @@ $(document).ready(function() {
 const url='https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json';
 const svgWidth = $(window).width()*0.9;  // width of plot area
 const svgHeight = $(window).height()*0.75;  //height of plot area
+const title = "History of Meteor Landings";
+const subtitle = "";
 
-const forceRatio = (1000-svgWidth)/5000;
-
-console.log(svgWidth, forceRatio);
+let titleDiv = d3.select(".title");
+titleDiv.append("h3")
+        .html(title);
+titleDiv.append("p")
+        .html(subtitle);
+    
+const projection = d3.geoMercator()
+    .scale(svgWidth/3)
+    .translate([svgWidth, svgHeight]);
+const path = d3.geoPath().projection(projection);;
 
 let svg = d3.select(".chart").append("svg")
     .attr("class", "chart-area")
     .attr("width", svgWidth)
-    .attr("height", svgHeight);
+    .attr("height", svgHeight)
     
-svg.append("image")
-        .attr("width", 1920)
-        .attr("height", 1629)
-        .attr("xlink:href", "http://blogs.discovermagazine.com/d-brief/files/2016/11/Mercator_projection_SW.jpg")
+
+const mapUrl = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson"
+
+d3.json(mapUrl, function(error, geojson) {
+    if (error) throw error;
+    let map = svg.append("path")
+        .attr("d", path(geojson));
+
+    function zoomed() {
+            map.attr("transform", d3.event.transform);
+          }
+        
+    const zoom = d3.zoom()
+        .scaleExtent([.4, 40])
+        .translateExtent([[-100, -100], [svgWidth + 1200, svgHeight + 500]])
+        .on("zoom", zoomed);
+    
+    svg.call(zoom);
+
+    
+    });
+        
 
 $.get(url, function(result) {
     handleData(result);
@@ -28,14 +55,6 @@ $.get(url, function(result) {
 
 const handleData = (data) => {
     console.log(data);
-    const title = "History of Meteor Landings";
-    const subtitle = "";
-
-    let titleDiv = d3.select(".title");
-    titleDiv.append("h3")
-        .html(title);
-    titleDiv.append("p")
-        .html(subtitle);
     
         /*
     //tooltip
@@ -53,4 +72,7 @@ const handleData = (data) => {
     */
 }
 
-}); 
+});
+
+
+
